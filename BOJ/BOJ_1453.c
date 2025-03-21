92efaa92f759004acba73b0d3b677d0ab3b9fd52
uncledrew-sr <bonggutravel@gmail.com>
2025-03-21 10:13:34 +0900

    add comment

--- BOJ/BOJ_1453.c
+++ BOJ/BOJ_1453.c
@@ -5,6 +5,7 @@ void useComputer(){
     scanf("%d", &N);
     int reject_num = 0;
     int computer[101] = {0}; // null 때문에 100이 아닌 101 사용
+    // {0, }과 {0}은 문법적으로 동일, 둘중 아무거나 사용해도 무방함
 
     while(N--){ // 손님의 수 N 만큼 반복하는 while loop
         int select_num;
