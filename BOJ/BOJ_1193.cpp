0306ba86a0533cc1243f0619517cbd98b596a89f
uncledrew-sr <bonggutravel@gmail.com>
2025-05-19 09:44:45 +0900

    0519 done

---
+++ BOJ/BOJ_1193.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int X;
+    cin >> X;
+
+    int i = 1;
+
+    while (X > i)
+    {
+        X -= i;
+        i++;
+    }
+
+    if (i % 2 == 0)
+        cout << X << "/" << i - X + 1;
+    else
+        cout << i - X + 1 << "/" << X;
+
+    return 0;
+}
