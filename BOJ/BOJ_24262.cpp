e96625be743c3438ec7005c88cc868ceedf8825c
uncledrew-sr <bonggutravel@gmail.com>
2025-05-27 08:51:08 +0900

    0527 done

---
+++ BOJ/BOJ_24262.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << 1 << "\n" << 0;
+    // 어떤 n을 넣더라도 코드는 1회 수행, 수행시간은 상수 시간이므로 시간복잡도는 O(1)
+    return 0;
+}
